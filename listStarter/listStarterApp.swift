de8ae9bd4ff32daf31dc1103a4f96e881b7194ec
Burcu Kamilcelebi <burcucelebi@gmail.com>
2023-08-15 13:33:43 +0300

    Initial Commit

---
+++ listStarter/listStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  listStarterApp.swift
+//  listStarter
+//
+//  Created by Burcu Kamilçelebi on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct listStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
